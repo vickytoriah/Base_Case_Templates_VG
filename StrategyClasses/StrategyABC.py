7c4269db269beda09a991f39621f1d5949dc84dc
vickytoriah <115563431+vickytoriah@users.noreply.github.com>
2024-11-21 18:13:21 +0000

    added doc strings into the abstract methods for the Queens tower's team's reference

--- StrategyClasses/StrategyABC.py
+++ StrategyClasses/StrategyABC.py
@@ -10,12 +10,24 @@ class StrategyBaseClass(ABC):
     def data_restructuring(
         self,
     ):
+        """
+        This should take in data (pd.DataFrame) in the raw cleaned data from a data pipeline,
+         and restructure the data in way that is suitable for the specific strategy
+        :return: restructured data, most likely a pd.DataFrame, with the content otherwise unchanged
+        """
         pass
 
     @abstractmethod
     def strategy_runner(
         self,
     ):
+        """
+        This should be the function that calls the method of generating the strategy signals, the execution points,
+        and the threshold adjusted trade entries and exit points.
+        Kept as one function for generalization of the sub-runner for the strategy
+        :return: Trade execution points with the position of each trade specified in buy sell signs (+/-);
+         the index should be a DatetimeIndex type and the columns, should include named multilevels
+        """
         pass
     
     
